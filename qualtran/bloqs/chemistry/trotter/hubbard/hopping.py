a3eac33e45fcee37f767323ad021a28a8d617d17
Fionn Malone <fmalone@google.com>
2024-03-04 20:01:32 +0000

    Clean up hopping.

--- qualtran/bloqs/chemistry/trotter/hubbard/hopping.py
+++ qualtran/bloqs/chemistry/trotter/hubbard/hopping.py
@@ -11,7 +11,24 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Bloqs implementing unitary evolution under the one-body hopping Hamiltonian."""
+r"""Bloqs implementing unitary evolution under the one-body hopping Hamiltonian in 2D.
+
+The hopping hamiltonian is given as 
+$$
+H_h = -\tau \sum_{\langle i, j\rangle} a_i^{\dagger} a_j + \mathrm{h.c.} 
+$$
+where the sum is over nearest neighbour lattice sites (under periodic boundary conditions).
+
+The plaquette splitting rewrites $H_h$ as a sum of $H_h^p$ and $H_h^g$ (for pink and gold 
+respectively) which when combined tile the entired lattice. Each plaquette
+contains four sites and paritions the lattice such that each edge of the lattice
+belongs to a single plaquette. Each term within a grouping commutes so that the
+unitary can be be implemented as
+$$
+e^{i H_h^{x}} = \prod_{k\sigma} e^{i H_h^{x(k,\sigma)}}
+$$
+without further trotter error.
+"""
 
 from functools import cached_property
 from typing import Set, TYPE_CHECKING, Union
@@ -28,9 +45,45 @@ if TYPE_CHECKING:
 
 @frozen
 class HoppingPlaquette(Bloq):
-    """Plaquette operator."""
+    r"""A bloq implementing a single plaquette unitary.
 
-    angle: Union[float, sympy.Expr]
+    The bloq implements
+    $$
+        e^{i \kappa R_\mathrm{plaq}}
+    $$
+    where $\tau R^{k\sigma}_\mathrm{plaq} = H_h^{x(k,\sigma)}$, i.e. R is
+    non-zero only in the subploq relevant for the particular indexed plaquette.
+
+    The plaquette operator is given by
+    $$
+        \sum_{i,j} [R_{\mathrm{plaq}}]_{i,j} a_{i\sigma}^\dagger a_{j\sigma}
+    $$
+    where the non-zero sub-bloq of R_{\mathrm{plaq}} is
+
+    $$
+        R_{\mathrm{plaq}} = 
+        \begin{bmatrix}
+            0 & 1 & 0 & 1 \\
+            1 & 0 & 1 & 0 \\
+            0 & 1 & 0 & 1 \\
+            1 & 0 & 1 & 0
+        \end{bmatrix}
+    $$
+
+    Args:
+        kappa: The scalar prefactor appearing in the definition of the unitary.
+            Usually a combination of the timestep and the hopping parameter $\tau$. 
+        eps: The precision of the single qubit rotations.
+
+    Registers:
+        qubits: A register of four qubits this unitary should act on.
+
+    References:
+        [Early fault-tolerant simulations of the Hubbard model](
+            https://arxiv.org/abs/2012.09238) page 13 Eq. E4 and E5 (Appendix E)
+    """
+
+    kappa: Union[float, sympy.Expr]
     eps: Union[float, sympy.Expr] = 1e-9
 
     @cached_property
@@ -39,34 +92,61 @@ class HoppingPlaquette(Bloq):
 
     def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
         # The FGate in the reference is F(k=0, n=arbitrary)
-        return {(FGate(0, 1), 4), (Rz(self.angle, eps=self.eps), 2)}
+        # page 14, discussion after E13
+        # There are 4 flanking f-gates and a e^{iXX}e^{iYY} rotation, which can
+        # be rotated to single rotation + cliffords.
+        return {(FGate(0, 1), 4), (Rz(self.kappa, eps=self.eps), 2)}
 
 
 @frozen
 class HoppingTile(Bloq):
     r"""Bloq implementing a "tile" of the one-body hopping unitary.
 
+    Implements the unitary
+    $$
+    e^{i H_h^{x}} = \prod_{k\sigma} e^{i t H_h^{x(k,\sigma)}}
+    $$
+    for a particular choise of of plaquette hamiltonian $H_h^x$, where $x$ = pink or gold.
+
     Args:
-        length: Lattice length L.
+        length: Lattice side length L.
+        angle: The prefactor scaling the Hopping hamiltonian in the unitary (`t` above).
+            This should contain any relevant prefactors including the time step
+            and any splitting coefficients.
+        tau: The Hopping hamiltonian parameter. Typically the hubbard model is
+            defined relative to $\tau$ so it's defaulted to 1.
+        eps: The precision of the single qubit rotations.
+        pink:
 
     Registers:
         system: The system register of size 2 `length`.
 
     References:
         [Early fault-tolerant simulations of the Hubbard model](
-            https://iopscience.iop.org/article/10.1088/2058-9565/ac3110/meta)
+            https://arxiv.org/abs/2012.09238) see Eq. 21 and App E.
     """
 
     length: Union[int, sympy.Expr]
     angle: Union[float, sympy.Expr]
+    tau: float = 1.0
     eps: Union[float, sympy.Expr] = 1e-9
+    pink: bool = True
+
+    def __attrs_post_init__(self):
+        if self.length % 2 != 0:
+            raise ValueError('Only even length lattices are supported')
+
+    def short_name(self) -> str:
+        l = 'p' if self.pink else 'g'
+        return f'H_h^{l}'
 
     @cached_property
     def signature(self) -> Signature:
         return Signature([Register('system', QAny(self.length), shape=(2,))])
 
     def build_call_graph(self, ssa: 'SympySymbolAllocator') -> Set['BloqCountT']:
-        return (HoppingPlaquette(), self.length**2 // 2)
+        # Page 5, text after Eq. 22. There are L^2 / 4 plaquettes of a given colour and x2 for spin.
+        return (HoppingPlaquette(kappa=self.tau * self.angle, eps=self.eps), self.length**2 // 2)
 
 
 @bloq_example
